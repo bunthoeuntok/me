8e4ced093bf65c00355c22050bc3d0b5f999295a
Bunthoeun Tok <bunthoeun.code@gmail.com>
2026-07-13 09:43:52 +0700

    feat: update deployment workflow for GitHub Pages and add build-github script

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => ({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/me' : '/',
+  base: mode === 'github' ? '/me' : '/',
   server: {
     host: true,
     port: 3000,
