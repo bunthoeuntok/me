8ea3903b08803bc36dc337effe1d9a5942d9fc4d
Bunthoeun Tok <bunthoeun.code@gmail.com>
2026-07-13 09:24:47 +0700

    feat: update Vite configuration for production base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/',
+  base: command === 'build' ? '/me' : '/',
   server: {
     host: true,
     port: 3000,
   },
-})
+}))
